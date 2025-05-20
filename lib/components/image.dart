8d9c6fa8e609cb96d1c213701b61a2a2b09a5aaa
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-20 14:54:16 -0400

    componente - images, mas folder de assets con imagenes

--- lib/components/image.dart
+++ lib/components/image.dart
@@ -5,10 +5,58 @@ class ClassImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children:<Widget> [
-        Image.network('https://1000marcas.net/wp-content/uploads/2020/02/GitHub-logo-1.jpg'),
-        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+        const SizedBox(height: 40,),
+        const Center(
+          child: Text(
+            'Imagenes externas con "network"',
+              style: TextStyle(
+                backgroundColor: Colors.yellow,
+                fontSize: 20,
+                fontWeight: FontWeight.w500
+              ),
+            ),
+        ),
+        const SizedBox(height: 40,),
+
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Image.network('https://1000marcas.net/wp-content/uploads/2020/02/GitHub-logo-1.jpg'),
+              const SizedBox(height: 20,),
+              Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif'),
+            ],
+          ) 
+        ),
+        // ===================================================================================
+
+        const SizedBox(height: 40,),
+        const Center(
+          child: Text(
+            'Imagenes locales usando "assets"',
+              style: TextStyle(
+                backgroundColor: Colors.yellow,
+                fontSize: 20,
+                fontWeight: FontWeight.w500
+              ),
+          ),
+        ),
+        const SizedBox(height: 40,),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Image.asset('assets/images/imagen1.jpg'),
+              const SizedBox(height: 20,),
+              Image.asset('assets/images/imagen2.jpg'),
+            ],
+          )
+        ),
+
+
+
       ],
     );
   }
