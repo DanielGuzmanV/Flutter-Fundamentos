2723d80d9b19f80ca01fd158e9159bc04d575df1
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-26 18:53:45 -0400

    se agrego un contenedor para las importaciones

---
+++ lib/presentation/screens/container_screens.dart
@@ -0,0 +1,7 @@
+
+export 'package:basic_flutter/presentation/screens/buttons/buttons_screen.dart';
+export 'package:basic_flutter/presentation/screens/card/cards_screen.dart';
+export 'package:basic_flutter/presentation/screens/home/body_screen.dart';
+export 'package:basic_flutter/presentation/screens/home/home_screen.dart';
+
+
