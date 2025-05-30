ad4091e1518bcf61c8221a93f8f741992db3c8d9
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-30 15:39:51 -0400

    se agrego un snackbar

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -5,8 +5,35 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  // Metodo para mostrar el snackbar:
+  void showCustomSnackbar(BuildContext context) {
+
+    // Limpiamos los snackbars:
+    ScaffoldMessenger.of(context).clearSnackBars();
+    
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: const Text('Saludos desde el snackbar'),
+        duration: const Duration(seconds: 2),
+        action: SnackBarAction(label: 'okay', onPressed: (){}),
+      )
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y dialogos'),
+      ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+
+    );
   }
 }
