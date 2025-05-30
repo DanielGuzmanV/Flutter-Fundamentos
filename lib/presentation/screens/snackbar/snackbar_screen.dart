cbd7be36aa1419ea2b2568e85307d13e0c6b3774
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-30 16:11:07 -0400

    se agrego las funciones del snackbar

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
   static const String nameclass = 'snackbar_screen';
@@ -7,7 +8,6 @@ class SnackbarScreen extends StatelessWidget {
 
   // Metodo para mostrar el snackbar:
   void showCustomSnackbar(BuildContext context) {
-
     // Limpiamos los snackbars:
     ScaffoldMessenger.of(context).clearSnackBars();
     
@@ -18,7 +18,31 @@ class SnackbarScreen extends StatelessWidget {
         action: SnackBarAction(label: 'okay', onPressed: (){}),
       )
     );
+  }
 
+  // Metodo para mostrar un dialogo personalizado:
+  void openDialogo(BuildContext context) {
+    showDialog(
+      context: context, 
+      builder: (context) => AlertDialog(
+        title: const Text('Estas seguro?'),
+        content:  const Text('Fugiat esse magna amet qui. Id amet amet consequat magna aliquip esse reprehenderit id et veniam. Fugiat irure irure aute nostrud consectetur excepteur. In ex aliquip ad anim duis consequat ad consequat magna eiusmod. Id Lorem nulla voluptate veniam nostrud.'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Cancelar')
+          ),
+
+          FilledButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Aceptar')
+          )
+        ],
+      ),
+
+      // Evitamos que el cuadro de dialogo es cierre:
+      barrierDismissible: false,
+    );
   }
 
   @override
@@ -27,6 +51,37 @@ class SnackbarScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Snackbars y dialogos'),
       ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            // Button para mostrar licencias:
+            FilledButton.tonal(
+              child: const Text('Licencias usadas'),
+              onPressed: (){
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('Ad amet velit officia qui labore duis ex amet. Enim enim aliqua do et sit duis quis. Aliquip est qui ad dolor anim reprehenderit fugiat labore magna commodo reprehenderit mollit ad. Laborum voluptate adipisicing elit laborum aliqua irure.')
+                  ]
+                );
+              }, 
+            ),
+            // ================================================
+
+            // Button personalizado:
+            FilledButton.tonal(
+              child: const Text('Mostrar dialogo'),
+              onPressed: () => openDialogo(context), 
+            ),
+            // =================================================
+
+          ],
+        ),
+      ),
+
 
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar Snackbar'),
