b0f88a079dc66aff6f39ed8a2431f7de2a7d9ccd
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-30 19:19:38 -0400

    switch para seleccionar un vehiculo

--- lib/presentation/screens/ui_controls/ui_controls_body.dart
+++ lib/presentation/screens/ui_controls/ui_controls_body.dart
@@ -13,6 +13,23 @@ class _UiControlsBodyState extends State<UiControlsBody> {
 
   bool isDeveloper = true;
   EnunTransportation selectTransportation = EnunTransportation.auto;
+  bool wantsBreakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
+
+
+  String obtenerTextoTransporte(EnunTransportation value) {
+    switch(value) {
+      case EnunTransportation.auto:
+        return 'Auto';
+      case EnunTransportation.barco:
+        return 'Barco';
+      case EnunTransportation.avion:
+        return 'Avion';
+      case EnunTransportation.submarino:
+        return 'Submarino';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,52 +46,84 @@ class _UiControlsBodyState extends State<UiControlsBody> {
           })
         ),
 
-        RadioListTile(
-          title: const Text('Auto'),
-          subtitle: const Text('Viajar por auto'),
-          value: EnunTransportation.auto, 
-          groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
-          onChanged: (value) {
-            return setState(() {
-              selectTransportation = EnunTransportation.auto;
-            });
-          },
+        ExpansionTile(
+          title: const Text('Vehiculo de transporte'),
+          subtitle: Text(obtenerTextoTransporte(selectTransportation)),
+          children: [
+
+            RadioListTile(
+              title: const Text('Auto'),
+              subtitle: const Text('Viajar por auto'),
+              value: EnunTransportation.auto,
+              groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
+              onChanged: (value) {
+                return setState(() {
+                  selectTransportation = EnunTransportation.auto;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Avion'),
+              subtitle: const Text('Viajar por avion'),
+              value: EnunTransportation.avion,
+              groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
+              onChanged: (value) {
+                return setState(() {
+                  selectTransportation = EnunTransportation.avion;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Barco'),
+              subtitle: const Text('Viajar por barco'),
+              value: EnunTransportation.barco,
+              groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
+              onChanged: (value) {
+                return setState(() {
+                  selectTransportation = EnunTransportation.barco;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Submarino'),
+              subtitle: const Text('Viajar por submarino'),
+              value: EnunTransportation.submarino,
+              groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
+              onChanged: (value) {
+                return setState(() {
+                  selectTransportation = EnunTransportation.submarino;
+                });
+              },
+            ),
+
+          ],
         ),
 
-        RadioListTile(
-          title: const Text('Avion'),
-          subtitle: const Text('Viajar por avion'),
-          value: EnunTransportation.avion, 
-          groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
-          onChanged: (value) {
-            return setState(() {
-              selectTransportation = EnunTransportation.avion;
-            });
-          },
+        CheckboxListTile(
+          title: const Text('Incluir desayuno?'),
+          subtitle: const Text('Gratis'),
+          value: wantsBreakfast, 
+          onChanged: (value) => setState(() {
+            wantsBreakfast = !wantsBreakfast;
+          }),
         ),
 
-        RadioListTile(
-          title: const Text('Barco'),
-          subtitle: const Text('Viajar por barco'),
-          value: EnunTransportation.barco, 
-          groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
-          onChanged: (value) {
-            return setState(() {
-              selectTransportation = EnunTransportation.barco;
-            });
-          },
+        CheckboxListTile(
+          title: const Text('Incluir almuerzo?'),
+          subtitle: const Text('de pago'),
+          value: wantsLunch, 
+          onChanged: (value) => setState(() {
+            wantsLunch = !wantsLunch;
+          }),
         ),
 
-        RadioListTile(
-          title: const Text('Submarino'),
-          subtitle: const Text('Viajar por submarino'),
-          value: EnunTransportation.submarino, 
-          groupValue: selectTransportation, // Podremos marcar la opciones seleccionada
-          onChanged: (value) {
-            return setState(() {
-              selectTransportation = EnunTransportation.submarino;
-            });
-          },
+        CheckboxListTile(
+          title: const Text('Incluir cena?'),
+          subtitle: const Text('de pago'),
+          value: wantsDinner, 
+          onChanged: (value) => setState(() {
+            wantsDinner = !wantsDinner;
+          }),
         ),
 
 
