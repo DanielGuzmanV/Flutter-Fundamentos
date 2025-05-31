eaaed0cab98f765716d55befb50e26de53b6638d
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-05-31 09:40:51 -0400

    se inicio la base del tutorial de la app

--- lib/presentation/screens/app_tutorial/tutorial_screen.dart
+++ lib/presentation/screens/app_tutorial/tutorial_screen.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
+// Clase para las imagenes:
+class SliderInfo {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  SliderInfo(this.title, this.caption, this.imageUrl);
+}
+
+final slidesList = <SliderInfo> [
+  SliderInfo('Busca la comida', 'Excepteur id incididunt incididunt fugiat et ad aliquip nulla.', 'assets/images/1.png'),
+  SliderInfo('Entrega rapida', 'Quis quis mollit laboris ex adipisicing sit duis pariatur.', 'assets/images/2.png'),
+  SliderInfo('Disfruta la comida', 'Aliqua labore et dolor consequat Lorem.', 'assets/images/3.png'),
+  SliderInfo('Disfruta la comida', 'Aliqua labore et dolor consequat Lorem.', 'assets/images/3.png'),
+  SliderInfo('Disfruta la comida', 'Aliqua labore et dolor consequat Lorem.', 'assets/images/3.png'),
+  SliderInfo('Disfruta la comida', 'Aliqua labore et dolor consequat Lorem.', 'assets/images/3.png'),
+];
+
+
 class TutorialScreen extends StatelessWidget {
   static const String nameclass = 'tutorial_screen';
   
@@ -7,6 +26,41 @@ class TutorialScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        physics: const BouncingScrollPhysics(),
+        children: slidesList.map((slideDate) => _ClaseSlide(
+          title: slideDate.title, 
+          caption: slideDate.caption, 
+          imageUrl: slideDate.imageUrl
+        )).toList(),
+      ),
+    );
+  }
+}
+
+
+class _ClaseSlide extends StatelessWidget {
+  final String title;
+  final String caption;
+  final String imageUrl;
+
+  const _ClaseSlide({ 
+    required this.title, 
+    required this.caption, 
+    required this.imageUrl
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
+
+
+
+
+
+
+
+
