339a58b500620eeabdb32dded10504d9c38fcb55
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-02 15:21:51 -0400

    se realizo el menu lateral de la app

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:basic_flutter/presentation/screens/home/body_screen.dart';
+import 'package:basic_flutter/presentation/widgets/side_menu.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,6 +16,7 @@ class HomeScreen extends StatelessWidget {
       ),
 
       body: const BodyScreen(),
+      drawer: const SideMenu(),
 
     );
   }
---
+++ lib/presentation/widgets/side_menu.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class SideMenu extends StatefulWidget {
+  const SideMenu({super.key});
+
+  @override
+  State<SideMenu> createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+
+  int navDrawerIndex = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationDrawer(
+      selectedIndex: navDrawerIndex,
+      onDestinationSelected: (value) {
+        setState(() {
+          navDrawerIndex = value;
+        });
+      },
+
+      children: [
+        NavigationDrawerDestination(
+          icon: const Icon(Icons.home_outlined), 
+          label: const Text('Home drawer')
+        ),
+
+        NavigationDrawerDestination(
+          icon: const Icon(Icons.add_shopping_cart_rounded), 
+          label: const Text('Nueva pantalla')
+        ),
+      ]
+    );
+  }
+}
