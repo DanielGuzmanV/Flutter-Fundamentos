7f4ce86b0beb344b4454d48623d3577557f580f8
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-03 17:10:55 -0400

    se agrego cambio de temas y modo oscuro para toda la app

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,15 +3,21 @@ import 'package:flutter/material.dart';
 class AppTheme {
 
   final int valueColor;
+  final bool isDarkmode;
 
   AppTheme({
-    this.valueColor = 0
+    this.valueColor = 0,
+    this.isDarkmode = false,
+
   }): assert(valueColor >= 0, 'El color seleccionado debe ser mayor que 0'),
       assert(valueColor < colorList.length,'Selecciona un color entre el 0 y ${colorList.length -1 }');
 
   ThemeData seleccionarColor() {
     return ThemeData(
       useMaterial3: true,
+
+      brightness: isDarkmode? Brightness.dark : Brightness.light,
+
       colorSchemeSeed: colorList[valueColor],
       appBarTheme: const AppBarTheme(
         centerTitle: true
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:basic_flutter/config/router/app_router.dart';
+import 'package:basic_flutter/presentation/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:basic_flutter/config/theme/app_theme.dart';
@@ -13,18 +14,22 @@ void main() {
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    final bool valueDarkMode = ref.watch(isDarkmodeProvider);
+    final int selectcolor = ref.watch(selectedColorProvider);
+
     return MaterialApp.router(    
       
       routerConfig: appRouter,
 
       debugShowCheckedModeBanner: false,
       title: 'Fundamentos basicos flutter',
-      theme: AppTheme(valueColor: 6).seleccionarColor(),
+      theme: AppTheme(valueColor: selectcolor, isDarkmode: valueDarkMode).seleccionarColor(),
 
     );
   }
