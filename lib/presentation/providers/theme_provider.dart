ab31e84696051f98d9bedfb758c8aee8a420bef5
DLGuzBuho <109259673+DanielGuzmanV@users.noreply.github.com>
2025-06-03 16:30:26 -0400

    se creo un provider para los temas

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,4 +1,7 @@
+import 'package:basic_flutter/config/theme/app_theme.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final isDarkmodeProvider = StateProvider<bool>((ref) => false,);
 
+// Lista de colores
+final colorListProvider = Provider((ref) => colorList,);
